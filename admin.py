5a105c3daf4bb520c536778615c73fc702831c83
shadyabhi <abhijeet.1989@gmail.com>
2011-03-24 20:55:11 +0530

    Added README, new pylogin.db, changed dishes table

--- admin.py
+++ admin.py
@@ -1,8 +1,12 @@
 from pycourt_login.models import *
 from django.contrib import admin
 
+class DishesAdmin(admin.ModelAdmin):
+	list_display = ('dish_name','dish_price')
+	search_fields = ('dish_name',)
+
 admin.site.register(UserProfile)
-admin.site.register(Dishes)
+admin.site.register(Dishes,DishesAdmin)
 admin.site.register(Order)
 admin.site.register(BalanceAccount)
 admin.site.register(CounterAccount)
